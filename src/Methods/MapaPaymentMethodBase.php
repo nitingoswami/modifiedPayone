207b30faefd445d7088804d6d966d8a2511ea9bc
Sukhvinder Pal Singh <spsingh@athenasoft.in>
2019-06-26 13:29:27 +0530

    wsBcBAABCAAQBQJdEyXfCRBK7hj4Ov3rIwAAdHIIAGHrFeO/VSCyVfDyBN70NtsM  yFi/KXTEz4P7qucm+axWI4i+2qph+2Z8TP/5W3rxjhNfUiA2vWoqhwDstZ8BEzGB  QXlqndfNOrnUWGViO9jAnEAJOZ4r/5Cn81Gn+hPSstrxYZxPDT5ek5vlyHLRreqj  ddc52JlOT/AIcHLwcFA+b3lNUyJ6tZnqP7zPJjNQbK+YB5f6AqV6xs0kErw+x4Yl  a9CG4nV77XdkMZ27WhSOtLJ065e2JS0RYTNMZA9t8TR/Ulwm8Q8+IJcZQOP3qe3d  tRuXh1udKXtcx4+LarO0ryOOjPdQ7tJxCrYlMgf5OzwMJ2h5GcXCYKdmXBXrmEo=  =VdbK  -----END PGP SIGNATURE-----
    syntax error fix

--- src/Methods/MapaPaymentMethodBase.php
+++ src/Methods/MapaPaymentMethodBase.php
@@ -6,7 +6,7 @@ use Plenty\Modules\Account\Contact\Contracts\ContactRepositoryContract;
 use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
 use Plenty\Modules\Payment\Method\Contracts\PaymentMethodService;
 use Plenty\Plugin\ConfigRepository;
-use Plenty\Plugin\Log\Loggable
+use Plenty\Plugin\Log\Loggable;
 use Plenty\Plugin\Application;
 
 /**
