92874e8fc3dac96852d2d0f47a22d934fd74cfd0
Sukhvinder Pal Singh <spsingh@athenasoft.in>
2019-06-27 14:13:26 +0530

    wsBcBAABCAAQBQJdFIGuCRBK7hj4Ov3rIwAAdHIIAJK34SQF31lV8sgXUupfnErz  T+UW3wb2aj3pMJ3269Zb+FOTd+8AdZgQgl6M0ONB/4qPYH/Lo61qekI2c3WwRoy6  rvrtdrgxb3oimekjEEvX3XylhKY6Equ4/HfvfxAj6VHXJ4F42a8/xRq3eb1DAEqt  oA3Z8SQXqg0q6rA60rZOVg21yBPGyTIRy9mRZvLYfu6b/RYzeXongcVLTgpNv2Qo  rzTYwPP8od0EvmqkCYEOZDT772Kpjtgme5A7mJUvmYshBb/IUo9o5wJP8f6CKtRl  jUYWLZ+6mn/ZFlV2PtFUr0H+0FHgDpaq+oXuudnVQKQOpKW2G9AqvtTtUkBGVv0=  =IBYH  -----END PGP SIGNATURE-----
    nine

--- src/Controllers/MapaResponseController.php
+++ src/Controllers/MapaResponseController.php
@@ -102,7 +102,14 @@ class MapaResponseController extends Controller
       $this->sessionStorage->setSessionValue('lastPS', $_GET['ps']);
       $this->sessionStorage->setSessionValue('lastPR', $_GET['pr']);
         
-       return  $this->errorContainer->call();
+       $status = $this->sessionStorage->getSessionValue('lastPS');
+        $result = $this->sessionStorage->getSessionValue('lastPR');
+        if (!empty($status)){
+          $errorMSG = $status.': '.$result;
+          $this->sessionStorage->setSessionValue('lastPS', NULL);
+          $this->sessionStorage->setSessionValue('lastPR', NULL);
+          return $twig->render('Masterpayment::content.error', ['errorText' => $errorMSG]);
+        } 
       //return $this->response->redirectTo('checkout');
     }
     
